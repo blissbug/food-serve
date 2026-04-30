20105d80044b3d2bd97a3cc035b764d64e78b5b8
Dishaa Patni <dishaapatni8@gmail.com>
2026-04-30 16:30:17 +0530

    Initial Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+  <div className="h-screen bg-black flex items-center justify-center">
+  <h1 className="text-white text-3xl font-bold">
+    Tailwind is working 🚀
+  </h1>
+</div>
+    </>
+  )
+}
+
+export default App
