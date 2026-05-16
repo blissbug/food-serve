5963df3529738dc903672ca78b1b84766046ab94
blissbug <4385anupriyasachan@gmail.com>
2026-05-16 13:38:45 +0530

    update routes and use route grouping

--- backend/internal/user/routes.go
+++ backend/internal/user/routes.go
@@ -3,9 +3,10 @@ package user
 import "github.com/gin-gonic/gin"
 
 func (h *Handler) RegisterRoutes(s *gin.Engine) {
-	s.POST("/login/send-otp", h.HandleLogin)
-	s.POST("/login/verify", h.HandlerVerifyLogin)
-	s.POST("/auth/refresh", h.HandleRefresh)
-	s.POST("/register/send-otp", h.HandleRegister)
-	s.POST("/register/verify", h.HandleVerifyRegistration)
+	authGroup := s.Group("/auth")
+	authGroup.POST("/login/send-otp", h.HandleLogin)
+	authGroup.POST("/login/verify", h.HandlerVerifyLogin)
+	authGroup.POST("/refresh", h.HandleRefresh)
+	authGroup.POST("/register/send-otp", h.HandleRegister)
+	authGroup.POST("/register/verify", h.HandleVerifyRegistration)
 }
